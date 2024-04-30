4e84d0ddde4e9708c96144e0c882c3701c971fb1
Ru Hsuan Ding <rhding@C02CK76F-66bc1c.local>
2024-04-30 12:41:16 -0600

    finish up step 1

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ function createSubscriber(firstName, lastName, street1, street2, city, state) {
   subscribers.street2 = street2;
   subscribers.city = city;
   subscribers.state = state;
+
+  if (subscribers.state === "Utah") {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
 }
 
 createSubscriber(
@@ -28,9 +34,31 @@ createSubscriber(
   "84129",
   "123"
 );
+console.log(subscribers);
 
-if (subscribers.state === "Utah") {
-  console.log("true");
-} else {
-  console.log("false");
+function updateSubscripber(subscribers, newState) {
+  subscribers.city = newState;
+}
+
+const newState = "California";
+console.log(newState);
+
+let subscriber = [
+  //use brackets for array list
+  "Jeff",
+  "John",
+  "Jason",
+  "Sam",
+  "Clark",
+  "Jackson",
+  "Sarah",
+  "Molly",
+  "Adam",
+  "Levi",
+];
+
+function subscriberInfo(subscriber) {
+  for (let i = 0; i < subscriber.length; i++) {
+    console.log(subscriber[i]); //allows to access each subscriber object in the array
+  }
 }
