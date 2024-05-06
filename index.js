f6eb8a715f7ad160a2bc3a11d12a076dcf21fd7a
Ru Hsuan Ding <rhding@C02CK76F-66bc1c.local>
2024-05-06 08:47:08 -0600

    add all age for all subscribers

--- index.js
+++ index.js
@@ -209,8 +209,14 @@ function getAllAge(min, max) {
     resolve(Math.floor(Math.random() * max));
   }, min);
 }
-async function getAllSubscribers() {
-  age = await getAllAge(18, 80);
-  const root = subscribersArray.map();
-  subscribersArray.push(root);
+async function addAgeToAllSubscriber(subscribersArray) {
+  for (let i = 0; i < subscribersArray.length; i++) {
+    const existingSubscriber = subscribersArray[i];
+    existingSubscriber.age = await getAllAge(18, 80);
+  }
 }
+async function main1() {
+  await addAgeToAllSubscriber(subscribersArray);
+  console.log(subscribersArray);
+}
+main1();
